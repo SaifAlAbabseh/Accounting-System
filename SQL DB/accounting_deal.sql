02dcc9adbf047b45cd25a1b97e45c0bf0a9a24a1
FCBAYERN4EVER\sabab <sababseh97@gmail.com>
2024-03-02 20:15:21 +0300

    .

--- SQL DB/accounting_deal.sql
+++ SQL DB/accounting_deal.sql
@@ -12,11 +12,11 @@ CREATE TABLE `products` (
   `product_id` int(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `admin_id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `buy_price` decimal(10,2) NOT NULL,
+  `buy_price` decimal(65,2) NOT NULL,
   `quantity` int(255) NOT NULL,
-  `tax` decimal(10,2) NOT NULL,
-  `discount` decimal(10,2) NOT NULL,
-  `selling_price` decimal(10,2) NOT NULL,
+  `tax` decimal(65,2) NOT NULL,
+  `discount` decimal(65,2) NOT NULL,
+  `selling_price` decimal(65,2) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
